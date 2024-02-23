c86e1a653391c9a7f1682473ee5b3d3b801e01fa
levitskya03 <75023086+levitskya03@users.noreply.github.com>
2024-02-23 19:16:30 +0300

    add project

---
+++ gradio_ui.py
@@ -0,0 +1,29 @@
+import gradio as gr
+from utils import return_generated_tasks_docx
+
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("Введите данные об учениках и нажмите кнопку 'Сгенерировать файл с заданиями', далее нажмите на файл "
+                "'Задания', чтобы скачать его ")
+    with gr.Row():
+        inputs = [gr.Dataframe(
+
+            label="Таблица с оценками",
+            headers=["Номер", "ФИО Ученика", "Темы", "Предмет", "Количество заданий"],
+            datatype=["number", "str", "str", "str", "number"],
+            row_count=1,
+            col_count=(5, 'fixed'),
+        ),
+            gr.Dropdown(
+                choices=[i for i in range(1, 12)],
+                label="Выберите класс обучения для учеников"
+            )]
+        outputs = gr.File()
+
+    btn = gr.Button("Сгенерировать файл с заданиями")
+    btn.click(fn=return_generated_tasks_docx, inputs=inputs, outputs=outputs)
+
+
+demo.launch()
+
