2ca2881b7ad16ca040b87faf6b095b17f6de3633
levitskya03 <75023086+levitskya03@users.noreply.github.com>
2024-03-22 13:47:10 +0300

    final version

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,8 +1,6 @@
 import gradio as gr
 from utils import return_generated_tasks_docx
 
-
-
 with gr.Blocks() as demo:
     gr.Markdown("Введите данные об учениках и нажмите кнопку 'Сгенерировать файл с заданиями', далее нажмите на файл "
                 "'Задания', чтобы скачать его ")
@@ -10,20 +8,21 @@ with gr.Blocks() as demo:
         inputs = [gr.Dataframe(
 
             label="Таблица с оценками",
-            headers=["Номер", "ФИО Ученика", "Темы", "Предмет", "Количество заданий"],
-            datatype=["number", "str", "str", "str", "number"],
+            headers=["Номер", "ФИО Ученика", "Темы", "Предмет", "Количество заданий", "Язык"],
+            datatype=["number", "str", "str", "str", "number", "str"],
             row_count=1,
-            col_count=(5, 'fixed'),
+            col_count=(6, 'fixed'),
+            scale=4
         ),
             gr.Dropdown(
                 choices=[i for i in range(1, 12)],
                 label="Выберите класс обучения для учеников"
-            )]
+            ),
+            gr.Textbox(label="Дополнительные настройки для модели (на английском)")]
+
         outputs = gr.File()
 
     btn = gr.Button("Сгенерировать файл с заданиями")
     btn.click(fn=return_generated_tasks_docx, inputs=inputs, outputs=outputs)
 
-
 demo.launch()
-
--- utils.py
+++ utils.py
@@ -3,26 +3,37 @@ import requests
 from deep_translator import GoogleTranslator
 
 
-def generate_tasks(students_name, students_class, subject, amount_of_tasks, list_of_themes):
+def generate_tasks(students_name, students_class, subject, amount_of_tasks, list_of_themes, prompt):
     url = "http://localhost:1337/v1/chat/completions"
     body = {
         "model": "gpt-3.5-turbo-16k",
         "stream": False,
-        'messages': [{'role': 'system', 'content': f'You are a good {subject} teacher in school in {students_class} grade. '},
+        'messages': [{'role': 'system',
+                      'content': f'You are a good {subject} teacher in school in {students_class} grade. {prompt} '},
                      {'role': 'user',
                       'content': f'Generate{amount_of_tasks} {subject} tasks for {students_name} in {students_class} grade'
-                                 f'who does not understand following themes: {list_of_themes}. Also generate tasks on close themes to revise material, but dont forget that students are in {students_class} grade '}]}
+                                 f'who does not understand following themes: {list_of_themes}. Also generate tasks on '
+                                 f'close themes to revise material, but dont forget that students are in '
+                                 f'{students_class} grade '}]}
     json_response = requests.post(url, json=body).json().get('choices', [])
     return json_response
 
-def return_generated_tasks_docx(dataframe, class_level):
+
+def return_generated_tasks_docx(dataframe, class_level, additional_prompt):
     doc = Document()
     file_name = "Готовые_задания.docx"
     for i in range(dataframe.shape[0]):
-        generated_content = generate_tasks(students_name=dataframe["ФИО Ученика"][i], students_class=class_level, subject=dataframe["Предмет"][i], amount_of_tasks=dataframe["Количество заданий"][i], list_of_themes=dataframe["Темы"][i])
+        generated_content = generate_tasks(students_name=dataframe["ФИО Ученика"][i], students_class=class_level,
+                                           subject=dataframe["Предмет"][i],
+                                           amount_of_tasks=dataframe["Количество заданий"][i],
+                                           list_of_themes=dataframe["Темы"][i], prompt=additional_prompt)
         for choice in generated_content:
-            doc.add_paragraph(GoogleTranslator(source='auto', target='ru').translate(choice.get('message', {}).get('content', '')))
+            language = dataframe["Язык"][i]
+            if language:
+                doc.add_paragraph(GoogleTranslator(source='auto', target=dataframe["Язык"][i]).translate(
+                    choice.get('message', {}).get('content', '')))
+            else:
+                doc.add_paragraph(GoogleTranslator(source='auto', target='ru').translate(
+                    choice.get('message', {}).get('content', '')))
     doc.save(file_name)
     return file_name
-
-
